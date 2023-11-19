e87dc064053fb8fbe29c2121df818db0b8c0fe46
VoronkovAleksandr <a_voron@list.ru>
2023-11-19 10:33:09 +0500

    HomeWork003 изменил метод assertEquals на assertTrue и asserFalse

--- HomeWork003/src/test/java/Task001Test.java
+++ HomeWork003/src/test/java/Task001Test.java
@@ -9,14 +9,14 @@ public class Task001Test {
     @DisplayName("Проверка при делении на четное число")
     void evenOddNumberIsTrue() {
         // Ожидаем true при передаче в метод четного числа
-        assertEquals(true, Task001.evenOddNumber(2));
+        assertTrue(Task001.evenOddNumber(2));
     }
 
     @Test
     @DisplayName("Проверка при делении на нечетное число")
     void evenOddNumberIsFalse() {
         // Ожидаем false при передаче в метод нечетного числа
-        assertEquals(false, Task001.evenOddNumber(5));
+        assertFalse(Task001.evenOddNumber(5));
     }
 
 }
--- HomeWork003/src/test/java/Task002Test.java
+++ HomeWork003/src/test/java/Task002Test.java
@@ -9,19 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
     @DisplayName("Проверка числом входящим в диапазон")
     void numberInInterval() {
         // Ожидаем true при передаче в метод числа из диапазона
-        assertEquals(true, Task002.numberInInterval(50));
+        assertTrue(Task002.numberInInterval(50));
     }
 
     @Test
     @DisplayName("Проверка числом больше диапазона")
     void numberOutOfIntervalPlus(){
         // Ожидаем false при передаче в метод числа больше диапазона
-        assertEquals(false, Task002.numberInInterval(101));
+        assertFalse (Task002.numberInInterval(101));
+
     }
     @Test
     @DisplayName("Проверка числом меньше диапазона")
     void numberOutOfIntervalMinus(){
         // Ожидаем false при передаче в метод числа меньше диапазона
-        assertEquals(false, Task002.numberInInterval(24));
+        assertFalse(Task002.numberInInterval(24));
     }
 }
